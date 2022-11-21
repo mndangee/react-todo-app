2adf046af197b9c17f8f82db714d1c461368c41a
김민혜 <kmhkgh@naver.com>
2022-11-21 16:36:25 +0900

    Feat: 컨테이너 박스 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
-import './assets/css/layout.scss';
+import React, { Component } from 'react';
 
-export default function App() {
-  return <div></div>;
+import './assets/css/layout.scss';
+import './App.css';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='container'>
+        <div className='todoBlock'>
+          <div className='title'>
+            <h1>할일 목록</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
