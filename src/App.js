2860e7bd85932759ee0b76081d3677060c688b85
김민혜 <kmhkgh@naver.com>
2022-11-22 16:57:10 +0900

    Refactor: 할일 목록 state로 관리

--- src/App.js
+++ src/App.js
@@ -4,17 +4,27 @@ import './assets/css/layout.scss';
 import './App.css';
 
 export default class App extends Component {
+  state = {
+    todoData: [
+      { id: '1', title: '공부하기', completed: true },
+      { id: '2', title: '출근하기', completed: true },
+      { id: '3', title: '퇴근하기', completed: false },
+      { id: '4', title: '잘 자기', completed: false },
+    ],
+  };
+
   btnStyle = {
     position: 'absolute',
     right: '0px',
     top: '20px',
-    padding: '5px 9px',
+    width: '20px',
+    height: '20px',
     color: '#fff',
     fontWeight: '700',
     backgroundColor: '#6178DC',
     border: 'none',
     borderRadius: '50%',
-    cusrsor: 'pointer',
+    cursor: 'pointer',
   };
 
   getStyle = () => {
@@ -22,21 +32,15 @@ export default class App extends Component {
       position: 'relative',
       padding: '20px 0px',
       fontSize: '15px',
-      lineHeight: '25px',
+      lineHeight: '20px',
       borderBottom: '1px solid #ccc',
       TextDecoder: 'none',
     };
   };
 
-  todoData = [
-    { id: '1', title: '공부하기', completed: true },
-    { id: '2', title: '출근하기', completed: true },
-    { id: '3', title: '퇴근하기', completed: false },
-    { id: '4', title: '잘 자기', completed: false },
-  ];
-
   deleteTodo = id => {
-    let newTodoData = this.todoData.filter(data => data.id !== id);
+    let newTodoData = this.state.todoData.filter(data => data.id !== id);
+    this.setState({ todoData: newTodoData }); // 상태값 업데이트
     console.log(newTodoData);
   };
 
@@ -47,7 +51,7 @@ export default class App extends Component {
           <div className='title'>
             <h1>할 일 목록</h1>
           </div>
-          {this.todoData.map(data => (
+          {this.state.todoData.map(data => (
             <div key={data.id} style={this.getStyle()}>
               <input
                 style={{ marginRight: '10px' }}
