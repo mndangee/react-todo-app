cfd09cc2b013812474028d7b6a1fc0a7f762250e
김민혜 <kmhkgh@naver.com>
2022-11-22 16:33:02 +0900

    Feat: 클릭한 목록 삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -7,9 +7,10 @@ export default class App extends Component {
   btnStyle = {
     position: 'absolute',
     right: '0px',
-    top: '10px',
+    top: '20px',
     padding: '5px 9px',
     color: '#fff',
+    fontWeight: '700',
     backgroundColor: '#6178DC',
     border: 'none',
     borderRadius: '50%',
@@ -19,12 +20,24 @@ export default class App extends Component {
   getStyle = () => {
     return {
       position: 'relative',
-      padding: '10px',
+      padding: '20px 0px',
       fontSize: '15px',
       lineHeight: '25px',
       borderBottom: '1px solid #ccc',
       TextDecoder: 'none',
     };
+  };
+
+  todoData = [
+    { id: '1', title: '공부하기', completed: true },
+    { id: '2', title: '출근하기', completed: true },
+    { id: '3', title: '퇴근하기', completed: false },
+    { id: '4', title: '잘 자기', completed: false },
+  ];
+
+  deleteTodo = id => {
+    let newTodoData = this.todoData.filter(data => data.id !== id);
+    console.log(newTodoData);
   };
 
   render() {
@@ -34,15 +47,24 @@ export default class App extends Component {
           <div className='title'>
             <h1>할 일 목록</h1>
           </div>
-          <div style={this.getStyle()}>
-            <input
-              style={{ marginRight: '10px' }}
-              type='checkbox'
-              defaultChecked={false}
-            />
-            공부하기
-            <button style={this.btnStyle}>x</button>
-          </div>
+          {this.todoData.map(data => (
+            <div key={data.id} style={this.getStyle()}>
+              <input
+                style={{ marginRight: '10px' }}
+                type='checkbox'
+                defaultChecked={data.completed}
+              />
+              {data.title}
+              <button
+                style={this.btnStyle}
+                onClick={() => {
+                  this.deleteTodo(data.id);
+                }}
+              >
+                x
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     );
