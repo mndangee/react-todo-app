1d9e908e9565d2ff15dc8dbbc51aae2f5cb5e76e
김민혜 <kmhkgh@naver.com>
2022-11-22 15:57:21 +0900

    Feat: 목록 list UI 작업

--- src/App.js
+++ src/App.js
@@ -4,12 +4,44 @@ import './assets/css/layout.scss';
 import './App.css';
 
 export default class App extends Component {
+  btnStyle = {
+    position: 'absolute',
+    right: '0px',
+    top: '10px',
+    padding: '5px 9px',
+    color: '#fff',
+    backgroundColor: '#6178DC',
+    border: 'none',
+    borderRadius: '50%',
+    cusrsor: 'pointer',
+  };
+
+  getStyle = () => {
+    return {
+      position: 'relative',
+      padding: '10px',
+      fontSize: '15px',
+      lineHeight: '25px',
+      borderBottom: '1px solid #ccc',
+      TextDecoder: 'none',
+    };
+  };
+
   render() {
     return (
       <div className='container'>
         <div className='todoBlock'>
           <div className='title'>
-            <h1>할일 목록</h1>
+            <h1>할 일 목록</h1>
+          </div>
+          <div style={this.getStyle()}>
+            <input
+              style={{ marginRight: '10px' }}
+              type='checkbox'
+              defaultChecked={false}
+            />
+            공부하기
+            <button style={this.btnStyle}>x</button>
           </div>
         </div>
       </div>
